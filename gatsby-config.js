dc79de56b306391ed75b5ff167129994c4ce0983
Abhi Diddigi <adiddigi@apple.com>
2020-03-21 01:10:44 -0700

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    title: `Blog | Ashwini Munji`,
+    name: `Blog - Ashwini`,
+    siteUrl: `https://blog.nimu.work`,
+    description: `Personal blog of Ashwini Munji Diddigi`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `Welcome to Ashwini's Blog.`,
       maxWidth: 652,
     },
     social: [
       {
-        name: `twitter`,
-        url: `https://twitter.com/narative`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/narative`,
-      },
-      {
         name: `instagram`,
-        url: `https://instagram.com/narative.co`,
+        url: `https://instagram.com/ashwinimunji`,
       },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
+        url: `https://www.linkedin.com/ashwinimunji`,
       },
     ],
   },
