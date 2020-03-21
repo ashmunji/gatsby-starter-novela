f58700500b196e26048c39b01c28bcfa1a3a0aca
Abhi Diddigi <adiddigi@apple.com>
2020-03-21 01:58:23 -0700

    remove hero image

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -10,7 +10,7 @@ import React from 'react';
 export default function Logo({ fill }) {
   return (
     <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="5%" cy="5%" r="4" fill={fill}/>
+      <circle cx="50%" cy="50%" r="4" fill={fill}/>
     </svg>
 
   );
