ea66b595512bd101b61b187b20555db79f604c1e
Abhi Diddigi <adiddigi@apple.com>
2020-03-21 02:21:12 -0700

    go back

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -9,9 +9,17 @@ import React from 'react';
  */
 export default function Logo({ fill }) {
   return (
-    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="50%" cy="50%" r="4" fill={fill}/>
-    </svg>
-
+    <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M15.41 16.09L10.83 11.5L15.41 6.91L14 5.5L8 11.5L14 17.5L15.41 16.09Z"
+      fill={fill}
+    />
+  </svg>
   );
 }
